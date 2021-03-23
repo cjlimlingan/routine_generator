42ffddc96cf849e356271126187e75a15a21c2a3
CJ Limlingan <cjlimlingan@gmail.com>
2021-03-23 17:42:44 +0800

    Create createDrillBank function, add drill bank arrays, intialize standard and skill bank

--- guitarApp.js
+++ guitarApp.js
@@ -1,3 +1,8 @@
+const standardDrills = [];
+const skillDrills = [];
+const chordDrills = [];
+const scaleDrills = [];
+
 const drillFactory = (name, type, range, tempo, description, interval, chordType) => {
     return {
         _name: name, 
@@ -59,6 +64,22 @@ const drillFactory = (name, type, range, tempo, description, interval, chordType
     }
 }
 
-/*const basicDexterity = drillFactory('Basic Dexterity Drill', 'Standard', [1,12], 120, 'Follow the 1 2 3 4 - 4 3 2 1 playing pattern.');
-const intermediateDexterity = drillFactory('Intermediate Dexterity Drill', 'Standard', [1,12], 150, 'Follow the 1 3 2 4 - 4 2 3 1 playing pattern.');
-const advanceDexterity = drillFactory('Advance Dexterity Drill', 'Standard', [1,12], 180, 'Do all possible playing patterns.'); */
+const createDrillBank = () => {
+    const basicDexterity = drillFactory('Basic Dexterity Drill', 'Standard', [1,12], 120, 'Follow the 1 2 3 4 - 4 3 2 1 playing pattern.');
+    standardDrills.push(basicDexterity);
+    const intermediateDexterity = drillFactory('Intermediate Dexterity Drill', 'Standard', [1,12], 150, 'Follow the 1 3 2 4 - 4 2 3 1 playing pattern.');
+    standardDrills.push(intermediateDexterity);
+    const advanceDexterity = drillFactory('Advance Dexterity Drill', 'Standard', [1,12], 180, 'Do all possible playing patterns.');
+    standardDrills.push(advanceDexterity);
+
+    const speedSkill = drillFactory('Speed Picking Drill', 'Skill', [1, 12], 60, 'Start with 1/8 notes, then 1/8 note triplets, then 1/16 notes, then 1/16 note pentuplets, then 1/16 note hexaplets.');
+    skillDrills.push(speedSkill);
+    const basicStretch = drillFactory('Basic Stretching Drill', 'Skill', [9, 1], 90, 'Stretch fingers 2, 3, and 4 by 1 fret each.');
+    skillDrills.push(basicStretch);
+    const advanceStretch = drillFactory('Advance Stretching Drill', 'Skill', [9, 1], 90, 'Stretch fingers 2, 3, and 4, by 2 frets each.')
+    skillDrills.push(advanceStretch);
+    const cagedRoots = drillFactory('CAGED Roots Drill', 'Skill', [0, 15], 120, 'Cycle through the cycle of fourths and find all the root notes, using the CAGED shapes.');
+}
+ 
+createDrillBank();
+console.log(standardDrills);
